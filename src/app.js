1ed469ea5dc8e97e56830118fc3249cf72ced9e8
tarutin <alexeytarutin@gmail.com>
2020-10-09 07:53:45 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', __dirname + '/views')
 
 app.use('/static', express.static(__dirname + '/static'))
 app.use(compression())
