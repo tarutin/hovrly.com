27bba56be419a4a292a13d755f5aaba9a174f106
tarutin <alexeytarutin@gmail.com>
2021-06-10 00:11:54 +0300

    current year

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,10 +3,14 @@ import './index.css'
 
 class Footer extends React.Component {
 
+    nowYear() {
+        return new Date().getFullYear();
+    }
+
     render() {
         return (
             <footer>
-                <span>&copy; Treasy OU, 2020</span>
+                <span>&copy; Treasy OU, 2018—{this.nowYear()}</span>
                 <a href='https://github.com/tarutin/hovrly' target='_blank' rel='noopener noreferrer'>GitHub</a>
             </footer>
         )
